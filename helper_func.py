857735b0bd1ef39810d617bd311c30e3d377ec76
putraicipiyey <88121146+putraicipiyey@users.noreply.github.com>
2022-05-01 17:44:30 +0800

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -11,7 +11,7 @@ from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
-from config import ADMINS, FORCE_SUB_CHANNEL, FORCE_SUB_GROUP, FORCE_SUB_CHANNEL1, FORCE_SUB_GROUP1
+from config import ADMINS, FORCE_SUB_CHANNEL, FORCE_SUB_GROUP, FORCE_SUB_CHANNEL1,
 
 
 async def is_subscribed(filter, client, update):
@@ -21,20 +21,24 @@ async def is_subscribed(filter, client, update):
         return True
     if not FORCE_SUB_GROUP:
         return True
-    if not FORCE_SUB_GROUP1:
-        return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True
     try:
-        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL, user_id=user_id)
         member = await client.get_chat_member(chat_id=FORCE_SUB_GROUP, user_id=user_id)
-        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL1, user_id=user_id)
-        member = await client.get_chat_member(chat_id=FORCE_SUB_GROUP1, user_id=user_id)    
     except UserNotParticipant:
         return False
-
-    return member.status in ["creator", "administrator", "member"]
+    try:
+        member = await client.get_chat_member(
+            chat_id=FORCE_SUB_CHANNEL, user_id=user_id
+    try:
+        member = await client.get_chat_member(
+            chat_id=FORCE_SUB_CHANNEL2, user_id=user_id
+        )
+    except UserNotParticipant:
+        return False
+    
+   return member.status in ["creator", "administrator", "member"]
 
 
 async def encode(string):
